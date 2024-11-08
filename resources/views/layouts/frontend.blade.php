88c8eb0011d07e31e7b94d137185059e02c42c3d
ahmed gabril <ajbryl110@gmail.com>
2024-11-08 05:01:08 +0200

    sw updte show button to istall1

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,128 +1,147 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() == 'ar'? 'rtl':'ltr' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ app()->getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
 
-      <title> gray-tec ahmed-gabril-compony-profile</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="manifest" href="/manifest.json"/>
+    <title> gray-tec ahmed-gabril-compony-profile</title>
 
-
-<!-- Add to homescreen for Chrome on Android -->
-            <meta name="mobile-web-app-capable" content="yes">
-            <meta name="application-name" content="PWA">
-            <link rel="icon" sizes="512x512" href="/icons/android/android-launchericon-512-512.png">
-            <meta name="description" content="شركه جراي تك لخدمات المواقع  والتطبيقات برمجه تصميم  تحسين محركات البحث">
-
-            <!-- Add to homescreen for Safari on iOS -->
-            <meta name="apple-mobile-web-app-capable" content="yes">
-            <meta name="apple-mobile-web-app-status-bar-style" content="black">
-            <meta name="apple-mobile-web-app-title" content="PWA">
-            <link rel="apple-touch-icon" href="/icons/ios/512.png">
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-
-         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
-        <link href="{{ asset('asset/frontend/custom.css') }}" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="manifest" href="/manifest.json" />
 
 
-        @livewireStyles
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="application-name" content="PWA">
+    <link rel="icon" sizes="512x512" href="/icons/android/android-launchericon-512-512.png">
+    <meta name="description" content="شركه جراي تك لخدمات المواقع  والتطبيقات برمجه تصميم  تحسين محركات البحث">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="PWA">
+    <link rel="apple-touch-icon" href="/icons/ios/512.png">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+    <link href="{{ asset('asset/frontend/custom.css') }}" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
-    </head>
-    <body class="font-sans text-gray-900 bg-gray-200  dark:bg-slate-900 dark:text-white  antialiased " style=" "
->
-        <div class="p-0 mt-0  ">
+    @livewireStyles
 
 
-          <livewire:frontend.navbar/>
+</head>
+
+<body class="font-sans text-gray-900 bg-gray-200  dark:bg-slate-900 dark:text-white  antialiased " style=" ">
+    <div class="p-0 mt-0  ">
 
 
-          <div id="install-prompt"> <p>Install this app on your device?</p> <button onclick="showInstallPrompt()">Install</button> </div>
+        <livewire:frontend.navbar />
 
 
-                <main class="mb-16">
-                    {{$slot}}
-                </main>
+        <div id="install-prompt" class="container mx-auto bg-gray-50 dark:bg-gray-700  p-4 rounded-md flex-col items-center">
+            <p class="text-gray-700 dark:text-gray-50  font-normal py-1">  هل تريد هذا الموقع يعمل كا تطبيق اصلي علي هذا الجهاز  لسهوله الاستخدام وا مكانيه الاتصال بدون انترنت</p>
+          <div class="flex justify-around">
+
+            <button class="text-green-600 p-2 rounded-md mt-8  hover:bg-gray-300 " onclick="showInstallPrompt()">تثبيت التطبيق</button>
+
+            <button class="text-red-700 p-2 rounded-md mt-8  hover:bg-gray-300 " onclick="hideInstallPrompt()"> الغاء</button>
 
 
-                <livewire:frontend.footer/>
+          </div>
 
         </div>
 
 
-        @livewireScripts
-
-        <script  >
-            document.addEventListener("DOMContentLoaded", function () {
-                initFlowbite();
+        <main class="mb-16">
+            {{ $slot }}
+        </main>
 
 
-                let deferredPrompt;
+        <livewire:frontend.footer />
 
-window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
-  deferredPrompt = event;
-  document.getElementById('install-prompt').style.display = 'block';
-});
-
-window.showInstallPrompt = function() {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-      deferredPrompt = null;
-    });
-  }
-};
+    </div>
 
 
+    @livewireScripts
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            initFlowbite();
+
+
+            let deferredPrompt;
+
+            window.addEventListener('beforeinstallprompt', (event) => {
+                event.preventDefault();
+                deferredPrompt = event;
+                document.getElementById('install-prompt').style.display = 'flex-col';
             });
 
 
 
+            window.showInstallPrompt = function() {
+                if (deferredPrompt) {
+                    deferredPrompt.prompt();
+                    deferredPrompt.userChoice.then((choiceResult) => {
+                        if (choiceResult.outcome === 'accepted') {
 
-                        document.addEventListener('livewire:navigated', () => {
-
-
-                            initFlowbite();
-
-
-                                },{ once: true });
-
-
-
-
-
-
-            if ('serviceWorker' in navigator) {
-                window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js')
-                    .then(registration => {
-                    console.log('Service Worker registered with scope:', registration.scope);
-                    }).catch(error => {
-                    console.error('Service Worker registration failed:', error);
+                          document.getElementById('install-prompt').style.display = 'none';
+                            console.log('User accepted the install prompt');
+                        } else {
+                            console.log('User dismissed the install prompt');
+                            document.getElementById('install-prompt').style.display = 'none';
+                        }
+                        deferredPrompt = null;
                     });
-                });
+                }
+            };
+
+
+            window.hideInstallPrompt = function(){
+              document.getElementById('install-prompt').style.display = 'none';
+
             }
 
 
+        });
+
+
+
+
+        document.addEventListener('livewire:navigated', () => {
+
+
+            initFlowbite();
+
+
+        }, {
+            once: true
+        });
 
 
 
 
 
-        </script>
-    </body>
+
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js')
+                    .then(registration => {
+                        console.log('Service Worker registered with scope:', registration.scope);
+                    }).catch(error => {
+                        console.error('Service Worker registration failed:', error);
+                    });
+            });
+        }
+    </script>
+</body>
+
 </html>
