6934deb5837680b4eec96a519f6cc8cc34f6938c
ahmed gabril <ajbryl110@gmail.com>
2024-11-13 07:06:04 +0200

    test logo222

--- resources/views/livewire/frontend/getlogo.blade.php
+++ resources/views/livewire/frontend/getlogo.blade.php
@@ -3,7 +3,7 @@
 
 
     <a href="{{route('home')}}" wire:navigate class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="{{asset('storage/'.$getdata?->logo)}}" class="w-[128px] h-[113px]"   alt="ahmed gabril Logo" />
+        <img src="{{asset('storage/'.$getdata?->logo)}}" class="w-[120px] h-[113px]"   alt="ahmed gabril Logo" />
         {{-- <span class="self-center text-sm font-semibold capitalize whitespace-nowrap dark:text-white">{{ $getdata?->compony_name}}</span> --}}
 
     </a>
